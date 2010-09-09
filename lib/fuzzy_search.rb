c806bbbabc0b816e5706b132175b4d9348beeac3
David Mike Simon <david.mike.simon@gmail.com>
2010-09-09 13:53:01 -0500

    Doing score calculation in SQL, allowing scoped access

--- lib/fuzzy_search.rb
+++ lib/fuzzy_search.rb
@@ -1,7 +1,7 @@
 # vim:ts=2:sw=2:et
 
 module FuzzySearch
-  
+
   def self.included(model)
     model.extend ClassMethods
     model.extend WordNormalizerClassMethod unless model.respond_to? "normalize"
@@ -42,12 +42,12 @@ module FuzzySearch
   end
 
   module ClassMethods
-
     def self.extended(model)
       model.class_eval do
         cattr_accessor :fuzzy_ref
         self.fuzzy_ref = model.name.underscore
         has_many fuzzy_trigram_type_symbol.to_s.tableize.to_sym
+        named_scope :fuzzy_find_scope, lambda { |words| generate_fuzzy_find_scope_params(words) }
       end
     end
 
@@ -57,7 +57,7 @@ module FuzzySearch
     def fuzzy_ref_table; fuzzy_ref.pluralize; end
     def fuzzy_trigram_type_symbol; (fuzzy_ref_type.to_s + "Trigram").gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }.to_sym; end
     def fuzzy_trigram_type; Kernel::const_get(fuzzy_trigram_type_symbol); end
-    
+
     def fuzzy_search_attributes(*properties)
       cattr_accessor :fuzzy_props
       self.fuzzy_props = properties
@@ -66,54 +66,42 @@ module FuzzySearch
     end
 
     def fuzzy_find(words)
-      unless words.instance_of? Array
-        # split the words on whitespaces and redo the find with that array
-        fuzzy_find(words.to_s.split(/[\s\-]+/))
-      else 
-        trigrams = []
-        words.each do |w|
-          word = ' ' + normalize(w) + ' '
-          word_as_chars = word.mb_chars
-          trigrams << (0..word_as_chars.length-3).collect {|idx| word_as_chars[idx,3].to_s}
-        end
-        trigrams = trigrams.flatten.uniq
+      fuzzy_find_scope(words).all
+    end
 
-        # Transform the list of columns in the searchable entity into 
-        # a SQL fragment like:
-        # "fuzzy_ref.id, fuzzy_ref.field1, fuzzy_ref.field2, ..."
-        #entity_fields = columns.map {|col| "fuzzy_ref." + col.name}.join(", ")
-        entity_fields = columns.map {|col| fuzzy_ref_table + "." + col.name}.join(", ")
-        
-        results = find( :all, 
-                        :select => "count(*) AS count, #{entity_fields}",
-                        :joins => ["LEFT OUTER JOIN #{fuzzy_ref}_trigrams ON #{fuzzy_ref}_trigrams.#{fuzzy_ref}_id = #{fuzzy_ref_table}.id"],
-                        :conditions => ["#{fuzzy_ref}_trigrams.token IN (?)", trigrams],
-                        :group => entity_fields,
-                        :order => "count DESC" )
+    private
 
-        logger.info "fuzzy_find query found #{results.size} results"
-        annotated_results = results.collect do |ref|
-          # put a weight on each instance for display purpose
-          def ref.fuzzy_weight=(w)
-            @weight = w
-          end
-          def ref.fuzzy_weight
-            @weight
-          end
-          
-          # if there are no double entries then
-          # i.count <= trigrams.size and i.count <= 'type'Trigrams.count
-          ref.fuzzy_weight = ((ref.count.to_i * 100)/trigrams.size +
-            (ref.count.to_i * 100)/fuzzy_trigram_type.send("count", :conditions => {fuzzy_ref_id => ref.id}))/2
-          logger.info "weight: #{ref.fuzzy_weight}"
-          ref
-        end
+    def generate_fuzzy_find_scope_params(words)
+      return {} unless words != nil
+      words = words.strip.to_s.split(/[\s\-]+/) unless words.instance_of? Array
+      return {} unless words.size > 0
 
-        # Remove the results that are too "far off" what the user intended
-        annotated_results.delete_if {|result| result.fuzzy_weight < fuzzy_threshold}
-
-        annotated_results
+      trigrams = []
+      words.each do |w|
+        word = ' ' + normalize(w) + ' '
+        word_as_chars = word.mb_chars
+        trigrams << (0..word_as_chars.length-3).collect {|idx| word_as_chars[idx,3].to_s}
       end
+      trigrams = trigrams.flatten.uniq
+
+      # Transform the list of columns in the searchable entity into 
+      # a SQL fragment like:
+      # "fuzzy_ref.id, fuzzy_ref.field1, fuzzy_ref.field2, ..."
+      entity_fields = columns.map {|col| fuzzy_ref_table + "." + col.name}.join(", ")
+
+      # The SQL expression for calculating fuzzy_weight
+      # Has to be used multiple times because some databases (i.e. Postgres) do not support HAVING on named SELECT fields
+      fuzzy_weight_expr = "(((count(*)*100.0)/#{trigrams.size}) + " +
+        "((count(*)*100.0)/(SELECT count(*) FROM #{fuzzy_ref}_trigrams WHERE #{fuzzy_ref}_id = #{fuzzy_ref_table}.id)))/2.0"
+
+      return {
+        :select => "#{fuzzy_weight_expr} AS fuzzy_weight, #{entity_fields}",
+        :joins => ["LEFT OUTER JOIN #{fuzzy_ref}_trigrams ON #{fuzzy_ref}_trigrams.#{fuzzy_ref}_id = #{fuzzy_ref_table}.id"],
+        :conditions => ["#{fuzzy_ref}_trigrams.token IN (?)", trigrams],
+          :group => entity_fields,
+          :order => "fuzzy_weight DESC",
+          :having => "#{fuzzy_weight_expr} >= #{fuzzy_threshold}"
+      }
     end
   end
 
